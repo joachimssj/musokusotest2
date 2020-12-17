64442936a3174ab2a2cc017d72486c811e7f8418
joachim yazigi <joachim.yazigi@gmail.com>
2020-12-17 01:24:56 +0100

    first test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState, useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const isSafari = () => {
+    const ua = navigator.userAgent.toLowerCase();
+    return ua.indexOf("safari") > -1 && ua.indexOf("chrome") < 0;
+};
+
+const mainVideo =
+    "https://cdn.shopify.com/s/files/1/0515/1293/4600/files/musokuso_comingsoon_online-video-cutter.com.mp4?v=1608128488";
+
+export default function App() {
+    const videoParentRef = useRef();
+    const [shouldUseImage, setShouldUseImage] = useState(false);
+    const [isMuted, setIsMuted] = useState(true);
+    const [isMutedMobile, setIsMutedMobile] = useState(true);
+
+    useEffect(() => {
+        // check if user agent is safari and we have the ref to the container <div />
+        if (isSafari() && videoParentRef.current) {
+            // obtain reference to the video element
+            const player = videoParentRef.current.children[0];
+
+            // if the reference to video player has been obtained
+            if (player) {
+                // set the video attributes using javascript as per the
+                // webkit Policy
+                player.controls = false;
+                player.playsinline = true;
+                player.muted = true;
+                player.setAttribute("muted", ""); // leave no stones unturned :)
+                player.autoplay = true;
+
+                // Let's wait for an event loop tick and be async.
+                setTimeout(() => {
+                    // player.play() might return a promise but it's not guaranteed crossbrowser.
+                    const promise = player.play();
+                    // let's play safe to ensure that if we do have a promise
+                    if (promise.then) {
+                        promise
+                            .then(() => {})
+                            .catch(() => {
+                                // if promise fails, hide the video and fallback to <img> tag
+                                videoParentRef.current.style.display = "none";
+                                setShouldUseImage(true);
+                            });
+                    }
+                }, 0);
+            }
+        }
+    }, []);
+
+    const pressVideo = () => {
+        const player = videoParentRef.current.children[0];
+
+        // if the reference to video player has been obtained
+        if (player) {
+            // set the video attributes using javascript as per the
+            // webkit Policy
+            player.controls = false;
+            player.playsinline = true;
+            player.muted = false;
+            player.setAttribute("muted", ""); // leave no stones unturned :)
+            player.autoplay = true;
+
+            // Let's wait for an event loop tick and be async.
+            setTimeout(() => {
+                // player.play() might return a promise but it's not guaranteed crossbrowser.
+                const promise = player.play();
+                // let's play safe to ensure that if we do have a promise
+                if (promise.then) {
+                    promise
+                        .then(() => {})
+                        .catch(() => {
+                            // if promise fails, hide the video and fallback to <img> tag
+                            videoParentRef.current.style.display = "none";
+                            setShouldUseImage(true);
+                        });
+                }
+            }, 0);
+        }
+    }
+
+    return shouldUseImage ? (
+        <img src={mainVideo} alt="Muted Video" />
+    ) : (
+        <div
+            onClick={() => pressVideo()}
+            ref={videoParentRef}
+            dangerouslySetInnerHTML={{
+                __html: `
+        <video
+          loop
+          muted
+          autoplay
+          playsinline
+          preload="metadata"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <source src="${mainVideo}" type="video/mp4" />
+        </video>`
+            }}
+        />
+    );
 }
-
-export default App;
